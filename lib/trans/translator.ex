f747ca2c183a95ad4b35103f383343c556252d3a
belaustegui <cristian.alvb@gmail.com>
2016-05-20 18:10:10 +0200

    Reorder `translate` parameters
    The translation container field is now received last, so it can be ommited if it is the default
    `:translations`.

--- lib/trans/translator.ex
+++ lib/trans/translator.ex
@@ -1,12 +1,14 @@
 defmodule Trans.Translator do
 
-  def translate(struct, translation_container, locale, field)
-  when is_atom(translation_container) and is_atom(locale) and is_atom(field) do
-    translate(struct, translation_container, to_string(locale), to_string(field))
+  def translate(struct, locale, field, translation_container \\ :translations)
+
+  def translate(struct, locale, field, translation_container)
+  when is_map(struct) and is_atom(translation_container) and is_atom(locale) and is_atom(field) do
+    translate(struct, to_string(locale), to_string(field), translation_container)
   end
 
-  def translate(struct, translation_container, locale, field)
-  when is_atom(translation_container) and is_binary(locale) and is_binary(field) do
+  def translate(struct, locale, field, translation_container)
+  when is_map(struct) and is_atom(translation_container) and is_binary(locale) and is_binary(field) do
     translated_field = with {:ok, all_translations} <- Map.fetch(struct, translation_container),
                             {:ok, translations_for_locale} <- Map.fetch(all_translations, locale),
                             {:ok, translated_field} <- Map.fetch(translations_for_locale, field),
