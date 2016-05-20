13956dcabad838e3f05bb0bbc3bbf1b9fa0608cc
belaustegui <cristian.alvb@gmail.com>
2016-05-20 18:27:01 +0200

    Simplify `translate` function
    The translate function now receives only the struct and the field to translate, every other
    parameter is passed in a `opts` keyword list. This allows us to omit the translations
    container if it is the default `:translations` and (in the future) omit the locale, which
    would be obtained from Gettext

--- lib/trans/translator.ex
+++ lib/trans/translator.ex
@@ -1,21 +1,17 @@
 defmodule Trans.Translator do
 
-  def translate(struct, locale, field, translation_container \\ :translations)
-
-  def translate(struct, locale, field, translation_container)
-  when is_map(struct) and is_atom(translation_container) and is_atom(locale) and is_atom(field) do
-    translate(struct, to_string(locale), to_string(field), translation_container)
-  end
-
-  def translate(struct, locale, field, translation_container)
-  when is_map(struct) and is_atom(translation_container) and is_binary(locale) and is_binary(field) do
+  def translate(struct, field, opts \\ []) when is_map(struct) do
+    translation_container = opts[:container] || :translations
+    locale = opts[:locale] # TODO Maybe get from gettext?
     translated_field = with {:ok, all_translations} <- Map.fetch(struct, translation_container),
-                            {:ok, translations_for_locale} <- Map.fetch(all_translations, locale),
-                            {:ok, translated_field} <- Map.fetch(translations_for_locale, field),
+                            {:ok, translations_for_locale} <- Map.fetch(all_translations, to_string(locale)),
+                            {:ok, translated_field} <- Map.fetch(translations_for_locale, to_string(field)),
       do: translated_field
     case translated_field do
       :error -> Map.fetch(struct, field) # Fallback to the default value
       _ -> translated_field # Return the translated value
     end
   end
+
+
 end
