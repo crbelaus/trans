257dc0ed0c8721f9a9a56f7e0a9ee582d9ca3c49
belaustegui <cristian.alvb@gmail.com>
2016-05-20 19:09:32 +0200

    Rename `with_translations_in` function
    The `with_translations_in` function has been renamed to `with_translations`
    to use the same nomenclature as Globalize[1]
    
    [1] https://github.com/globalize/globalize#scoping-objects-by-those-with-translations

--- lib/trans/query_builder.ex
+++ lib/trans/query_builder.ex
@@ -2,13 +2,13 @@ defmodule Trans.QueryBuilder do
   import Ecto.Query, only: [from: 2]
 
   # TODO add documentation
-  def with_translations_in(query, locale, opts \\ [])
+  def with_translations(query, locale, opts \\ [])
 
-  def with_translations_in(query, locale, opts) when is_atom(locale) do
-    with_translations_in(query, to_string(locale), opts)
+  def with_translations(query, locale, opts) when is_atom(locale) do
+    with_translations(query, to_string(locale), opts)
   end
 
-  def with_translations_in(query, locale, opts) do
+  def with_translations(query, locale, opts) do
     translation_container = opts[:container] || :translations
     from translatable in query,
       where: fragment("(?->>?) is not null", field(translatable, ^translation_container), ^locale)
