82ac851128e50d0728d8d95321fd353ce488b7cb
belaustegui <cristian.alvb@gmail.com>
2016-05-21 13:20:14 +0200

    Add convenience functions to query builder
    Three new functions have been added to the query builder. The function
    `with_translation_matching` checks if the given field translation matches
    exactly the desired value. The function `with_translation_like` checks if
    the given field translation matches the desired pattern. The function
    `with_translation_ilike` works as the previous one, but performs a case
    insensitive check.

--- lib/trans/query_builder.ex
+++ lib/trans/query_builder.ex
@@ -14,4 +14,50 @@ defmodule Trans.QueryBuilder do
       where: fragment("(?->>?) is not null", field(translatable, ^translations_container), ^locale)
   end
 
+  def with_translation_matching(query, locale, field, expected, opts \\ [])
+
+  # TODO add documentation
+  def with_translation_matching(query, locale, field, expected, opts)
+  when is_atom(locale) or is_atom(field) do
+    with_translation_matching(query, to_string(locale), to_string(field), expected, opts)
+  end
+
+  def with_translation_matching(query, locale, field, expected, opts)
+  when is_binary(locale) and is_binary(field) do
+    translations_container = opts[:container] || :translations
+    from translatable in query,
+      where: fragment("?->?->>?", field(translatable, ^translations_container), ^locale, ^field) == ^expected
+  end
+
+  # TODO add documentation
+  def with_translation_like(query, locale, field, pattern, opts \\ [])
+
+  def with_translation_like(query, locale, field, pattern, opts)
+  when is_atom(locale) or is_atom(field) do
+    with_translation_like(query, to_string(locale), to_string(field), pattern, opts)
+  end
+
+  def with_translation_like(query, locale, field, pattern, opts)
+  when is_binary(locale) and is_binary(field) do
+    translations_container = opts[:container] || :translations
+    from translatable in query,
+      where: like(fragment("?->?->>?", field(translatable, ^translations_container), ^locale, ^field), ^pattern)
+  end
+
+  # TODO add documentation
+  def with_translation_ilike(query, locale, field, pattern, opts \\ [])
+
+  def with_translation_ilike(query, locale, field, pattern, opts)
+  when is_atom(locale) or is_atom(field) do
+    with_translation_ilike(query, to_string(locale), to_string(field), pattern, opts)
+  end
+
+  def with_translation_ilike(query, locale, field, pattern, opts)
+  when is_binary(locale) and is_binary(field) do
+    translations_container = opts[:container] || :translations
+    from translatable in query,
+      where: ilike(fragment("?->?->>?", field(translatable, ^translations_container), ^locale, ^field), ^pattern)
+  end
+
+
 end
