767fe94847676c14a6eacadb9d00d4fae06947fe
belaustegui <cristian.alvb@gmail.com>
2016-05-21 17:31:07 +0200

    Add convenience functions to translated models
    Translated models can now use `Trans` and they will be extended with two
    functions (`with_translations` and `with_translation`) that act as a
    wrapper of `Trans.QueryBuilder` functions with predefined constants.

--- lib/trans.ex
+++ lib/trans.ex
@@ -1,2 +1,24 @@
 defmodule Trans do
+
+  defmacro __using__(opts) do
+    quote do
+
+      # Set up the translations container field
+      @translations_container unquote(opts[:container] || :translations)
+
+      # Wrapper of Trans.QueryBuilder.with_translations/3
+      def with_translations(query, locale, opts \\ []) do
+        Trans.QueryBuilder.with_translations(query, locale,
+          opts ++ [container: unquote(@translations_container)])
+      end
+
+      # Wrapper of Trans.QueryBuilder.with_translations
+      def with_translation(query, locale, field, expected, opts \\ []) do
+        Trans.QueryBuilder.with_translation(query, locale, field, expected,
+          opts ++ [container: unquote(@translations_container)])
+      end
+
+    end
+  end
+
 end
