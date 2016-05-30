9bc80efd984b0d108c58897a750f76711ca3a166
belaustegui <cristian.alvb@gmail.com>
2016-05-30 23:24:43 +0200

    Document Trans.QueryBuilder.with_translations/3

--- lib/trans/query_builder.ex
+++ lib/trans/query_builder.ex
@@ -1,7 +1,68 @@
 defmodule Trans.QueryBuilder do
   import Ecto.Query, only: [from: 2]
+  @moduledoc """
+  Provides functions to build queries with conditions on translated fields. Take
+  a look at the `Trans` module documentation to see how you can set up convenience
+  helpers in your model module to avoid repetition of default values.
+  """
 
-  # TODO add documentation
+  @doc """
+  Adds a condition to the given query to filter only those models translated to
+  the given locale.
+
+  Take a look at the `Trans` module to see how you can set up convenience functions
+  in your model to avoid excessive repetition of default options.
+
+  ## Usage example
+
+  Suppose that we have an article model which has a title and a body fields, both
+  of them can be translated.
+
+      defmodule Article do
+        use Ecto.Schema
+
+        schema "articles" do
+          field :title, :string
+          field :body, :string
+          field :translations, :map
+        end
+
+      end
+
+  We could then get only the articles that are translated into spanish like this:
+
+      iex> Article |> Trans.QueryBuilder.with_translations(:es) |> Repo.all
+      [debug] SELECT a0."id", a0."title", a0."body", a0."translations" FROM "articles" AS a0 WHERE ((a0."translations"->>$1) is not null) ["es"] OK query=4.7ms queue=0.1ms
+
+  ## Translation container
+
+  We may have some models in which translations are stored in a different column
+  than the default `translations`. When our translations container is not the
+  default one, it must be explicitly specified.
+
+  Suppose that we have a model like the one in the previous example, but which
+  stores the translations in the field `my_translation_container`:
+
+      defmodule Article do
+        use Ecto.Schema
+
+        schema "articles" do
+          field :title, :string
+          field :body, :string
+          field :my_translation_container, :map
+        end
+
+      end
+
+  Then, to get only the articles that are translated into Spanish we could use
+  the same technique as in the first example, but specifying the container:
+
+      iex> Article |> Trans.QueryBuilder.with_translations(:es, container: :my_translation_container) |> Repo.all
+      [debug] SELECT a0."id", a0."title", a0."body", a0."my_translation_container" FROM "articles" AS a0 WHERE ((a0."my_translation_container"->>$1) is not null) ["es"] OK query=4.7ms queue=0.1ms
+
+  You can avoid the same repetition by using the `Trans` module in your model
+  module. Take a look at the `Trans` module documentation to see how to do it.
+  """
   def with_translations(query, locale, opts \\ [])
 
   def with_translations(query, locale, opts) when is_atom(locale) do
