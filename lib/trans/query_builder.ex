e51689cafe2e61d6f6c33268b1acb0ea3fef62b9
belaustegui <cristian.alvb@gmail.com>
2017-04-02 15:03:01 +0200

    Add docs for Trans.QueryBuilder

--- lib/trans/query_builder.ex
+++ lib/trans/query_builder.ex
@@ -1,11 +1,81 @@
 if Code.ensure_loaded?(Ecto.Query) do
   defmodule Trans.QueryBuilder do
+    @moduledoc """
+    Adds conditions to `Ecto` queries on translated fields.
+    """
 
+    @doc """
+    Generates a SQL fragment for accessing a translated field in an `Ecto.Query`.
+
+    The generated SQL fragment can be coupled with the rest of the functions and
+    operators provided by `Ecto.Query` and `Ecto.Query.API`.
+
+    ## Safety
+
+    This macro will emit errors when used with untranslatable
+    schema modules or fields. Errors are emited during the compilation phase
+    thus avoiding runtime errors after the queries are built.
+
+    ## Usage examples
+
+    Imagine that we have an _Article_ schema declared as follows:
+
+        defmodule Article do
+          use Ecto.Schema
+          use Trans, translates: [:title, :body]
+
+          schema "articles" do
+            field :title, :string
+            field :body, :string
+            field :translations, :map
+          end
+        end
+
+    **Query for items that have a certain translation**
+
+    This `Ecto.Query` will return all _Articles_ that have an Spanish translation:
+
+        iex> Repo.all(from a in Article,
+        ...>   where: not is_nil(translated(Article, a, locale: :es)) )
+
+    The generated SQL is:
+
+        SELECT a0."id", a0."title", a0."body", a0."translations"
+        FROM "articles" AS a0
+        WHERE (NOT ((a0."translations"->"es") IS NULL))
+
+    **Query for items with a certain translated value**
+
+    This query will return all articles whose French title matches the _"Elixir"_:
+
+        iex> Repo.all(from a in Article,
+        ...>   where: translated(Article, a.title, locale: :fr) == "Elixir")
+
+    The generated SQL is:
+
+        SELECT a0."id", a0."title", a0."body", a0."translations"
+        FROM "articles" AS a0
+        WHERE ((a0."translations"->"fr"->>"title") = "Elixir")
+
+    **Query for items using a case insensitive comparison**
+
+    This query will return all articles that contain "elixir" in their Spanish
+    body, igoring case.
+
+        iex> Repo.all(from a in Article,
+        ...> where: ilike(translated(Article, a.body, locale: :es), "%elixir%"))
+
+    The generated SQL is:
+
+        SELECT a0."id", a0."title", a0."body", a0."translations"
+        FROM "articles" AS a0
+        WHERE ((a0."translations"->"es"->>"body") ILIKE "%elixir%")
+
+
+    """
     defmacro translated(module, translatable, opts) do
       with field <- field(translatable) do
-        Module.eval_quoted __CALLER__, [
-          __validate_fields__(module, field)
-        ]
+        Module.eval_quoted(__CALLER__, __validate_fields__(module, field))
         generate_query(schema(translatable), field, locale(opts))
       end
     end
